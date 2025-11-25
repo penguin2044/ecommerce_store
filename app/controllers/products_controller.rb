cc67c4dd9b15cd63a8d5e1eaf4f6da17bda8ad06
penguin2044 <glasstree25@gmail.com>
2025-11-25 12:54:50 -0600

    changed up the index page and added a category menu at the top

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,14 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.includes(:category).order(created_at: :desc)
+    if params[:category_id].present?
+      @category = Category.find(params[:category_id])
+      @products = @category.products.includes(:category).order(created_at: :desc)
+    else
+      @products = Product.includes(:category).order(created_at: :desc)
+      @category = nil
+    end
+
+    @categories = Category.all.order(:name)
   end
 
   def show

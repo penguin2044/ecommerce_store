1483ad82bb8488ea7f81e2930b477bc7613fab84
Jesse Kleinschmit <glasstree25@gmail.com>
2025-11-25 16:01:56 -0600

    added feature 2.5

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,14 @@
 class ProductsController < ApplicationController
   def index
     @categories = Category.all.order(:name)
-
+    
+    # Get featured and on-sale products for homepage
+    @featured_products = Product.featured.limit(4)
+    @on_sale_products = Product.on_sale.limit(4)
+    
     # Start with all products
     @products = Product.includes(:category)
-
+    
     # Filter by category if selected
     if params[:category_id].present?
       @category = Category.find(params[:category_id])
@@ -12,18 +16,18 @@ class ProductsController < ApplicationController
     else
       @category = nil
     end
-
+    
     # Filter by search query if present
     if params[:search].present?
       search_term = "%#{params[:search]}%"
       @products = @products.where(
-        "name ILIKE ? OR description ILIKE ?",
-        search_term,
+        "name ILIKE ? OR description ILIKE ?", 
+        search_term, 
         search_term
       )
       @search_query = params[:search]
     end
-
+    
     @products = @products.order(created_at: :desc)
   end
 

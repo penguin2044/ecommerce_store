ca95f595436a32c49ae319f1cf4bd6bd439ade83
Jesse Kleinschmit <glasstree25@gmail.com>
2025-11-26 15:04:41 -0600

    changed the seed to add 100 products to meet the category

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -1,18 +1,21 @@
 ActiveAdmin.register Page do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :title, :content, :slug
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:title, :content, :slug]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :title, :content, :slug
   
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :slug
+    column :created_at
+    actions
+  end
+  
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :slug
+      f.input :content, as: :text, input_html: { rows: 10 }
+    end
+    f.actions
+  end
 end
